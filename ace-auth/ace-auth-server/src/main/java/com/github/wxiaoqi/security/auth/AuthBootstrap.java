38af41c86e2c7e50f8395bfc282ad5313e1ef905
zhoujian <zhoujian@newtank.cn>
2018-12-28 13:45:56 +0800

    结构梳理

--- ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/AuthBootstrap.java
+++ ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/AuthBootstrap.java
@@ -10,14 +10,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
- * Created by Ace on 2017/6/2.
+ * @author tb
  */
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @MapperScan("com.github.wxiaoqi.security.auth.mapper")
 @RemoteApplicationEventScan(basePackages = "com.github.wxiaoqi.security.auth.common.event")
-@EnableAutoConfiguration
 public class AuthBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(AuthBootstrap.class, args);
