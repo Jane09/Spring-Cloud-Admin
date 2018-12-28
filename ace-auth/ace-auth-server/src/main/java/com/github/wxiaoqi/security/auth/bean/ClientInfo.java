724b46411888374a9c0f94a0e2ee45cc588533b2
zhoujian <zhoujian@newtank.cn>
2018-12-28 14:03:24 +0800

    结构梳理

--- ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/bean/ClientInfo.java
+++ ace-auth/ace-auth-server/src/main/java/com/github/wxiaoqi/security/auth/bean/ClientInfo.java
@@ -2,13 +2,18 @@ package com.github.wxiaoqi.security.auth.bean;
 
 
 import com.github.wxiaoqi.security.auth.common.util.jwt.IJWTInfo;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Created by ace on 2017/9/10.
  */
+@Getter
+@Setter
 public class ClientInfo implements IJWTInfo {
     String clientId;
     String name;
+    String id;
 
     public ClientInfo(String clientId, String name, String id) {
         this.clientId = clientId;
@@ -16,35 +21,8 @@ public class ClientInfo implements IJWTInfo {
         this.id = id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    String id;
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String getUniqueName() {
         return clientId;
-    }
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public String getName() {
-        return name;
     }
 }
