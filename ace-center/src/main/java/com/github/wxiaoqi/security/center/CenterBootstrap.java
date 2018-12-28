9581db26ae65dbddebd6d098e7383a4bda359334
zhoujian <zhoujian@newtank.cn>
2018-12-28 11:32:32 +0800

    结构梳理

--- ace-center/src/main/java/com/github/wxiaoqi/security/center/CenterBootstrap.java
+++ ace-center/src/main/java/com/github/wxiaoqi/security/center/CenterBootstrap.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * ${DESCRIPTION}
- *
- * @author wanghaobin
- * @create 2017-05-25 12:44
+ * @author tb
  */
-@EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaServer
 @SpringBootApplication
 public class CenterBootstrap {
     public static void main(String[] args) {
