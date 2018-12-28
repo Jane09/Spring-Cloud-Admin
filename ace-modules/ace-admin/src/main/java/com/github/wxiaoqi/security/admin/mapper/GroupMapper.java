01afb598c6fd1d47363a920681e4152339e86eea
zhoujian <zhoujian@newtank.cn>
2018-12-28 14:12:35 +0800

    结构梳理

--- ace-modules/ace-admin/src/main/java/com/github/wxiaoqi/security/admin/mapper/GroupMapper.java
+++ ace-modules/ace-admin/src/main/java/com/github/wxiaoqi/security/admin/mapper/GroupMapper.java
@@ -5,8 +5,12 @@ import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface GroupMapper extends Mapper<Group> {
-    public void deleteGroupMembersById (@Param("groupId") int groupId);
-    public void deleteGroupLeadersById (@Param("groupId") int groupId);
-    public void insertGroupMembersById (@Param("groupId") int groupId,@Param("userId") int userId);
-    public void insertGroupLeadersById (@Param("groupId") int groupId,@Param("userId") int userId);
+
+    void deleteGroupMembersById(@Param("groupId") int groupId);
+
+    void deleteGroupLeadersById(@Param("groupId") int groupId);
+
+    void insertGroupMembersById(@Param("groupId") int groupId, @Param("userId") int userId);
+
+    void insertGroupLeadersById(@Param("groupId") int groupId, @Param("userId") int userId);
 }
